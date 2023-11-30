9ff57f3f493b289be017fc88366e2346453e1668
AndrewSviatih <fagil.list@gmail.com>
2023-11-30 20:34:28 +0300

    final rgb_converter

---
+++ RGB_converter/RGB_counter.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+
+typedef struct {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+} Color;
+
+void get_color(Color *curr) {
+    scanf("%hhu%hhu%hhu", &curr->red, &curr->green, &curr->blue);
+}
+
+unsigned long colorToHTML(Color curr) {
+    return curr.red * pow(256, 2) + curr.green * 256 + curr.blue;
+}
+
+void print_color(Color rgb) {
+    printf("%hhu %hhu %hhu\n", rgb.red, rgb.green, rgb.blue);
+    printf("%lu\n", colorToHTML(rgb));
+    printf("%X%X%X", rgb.red, rgb.green, rgb.blue);
+}
+
+int main() {
+    Color test;
+
+    get_color(&test);
+    print_color(test);
+
+    return 0;
+}
