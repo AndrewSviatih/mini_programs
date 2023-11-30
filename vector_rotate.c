2a4885fa6161070c7884c3f73bc358f66bb9c65a
AndrewSviatih <fagil.list@gmail.com>
2023-11-30 15:14:56 +0300

    final

--- vector_rotate.c
+++ vector_rotate.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct{
     int x;
@@ -11,6 +12,22 @@ typedef struct {
     double vector_len;
 } Line;
 
+void scan_line(Line * t);
+void print_line(Line rotated);
+void rotate_line(Line * t);
+void count_vector(Line * t);
+
+int main () {
+    Line line;
+
+    scan_line(&line);
+    rotate_line(&line);
+    count_vector(&line);
+    print_line(line);
+
+    return 0;
+}
+
 void scan_line(Line * t){
     scanf("%d%d", &t->a.x, &t->a.y);
     scanf("%d%d", &t->b.x, &t->b.y);
@@ -20,6 +37,18 @@ void print_line(Line rotated){
     printf("%d %d %d %d %.3lf", rotated.a.x, rotated.a.y, rotated.b.x, rotated.b.y, rotated.vector_len);
 }
 
-int main (){
+void rotate_line(Line * t){
+    double start_x = t->a.x;
 
+    t->a.x = t->a.y;
+    t->a.y = -start_x;
+
+    start_x = t->b.x;
+
+    t->b.x = t->b.y;
+    t->b.y = -start_x;
+}
+
+void count_vector(Line * t){
+    t->vector_len = sqrt(pow(t->b.x - t->a.x, 2) + pow(t->b.y - t->a.y, 2));
 }
