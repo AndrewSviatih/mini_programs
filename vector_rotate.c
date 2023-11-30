85e5e979a0725038f0fc9e54227de42d92c615c0
AndrewSviatih <fagil.list@gmail.com>
2023-11-30 15:15:28 +0300

    final

--- vector_rotate.c
+++ vector_rotate.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-typedef struct{
+typedef struct {
     int x;
     int y;
 } Point;
@@ -12,12 +12,12 @@ typedef struct {
     double vector_len;
 } Line;
 
-void scan_line(Line * t);
+void scan_line(Line *t);
 void print_line(Line rotated);
-void rotate_line(Line * t);
-void count_vector(Line * t);
+void rotate_line(Line *t);
+void count_vector(Line *t);
 
-int main () {
+int main() {
     Line line;
 
     scan_line(&line);
@@ -28,16 +28,16 @@ int main () {
     return 0;
 }
 
-void scan_line(Line * t){
+void scan_line(Line *t) {
     scanf("%d%d", &t->a.x, &t->a.y);
     scanf("%d%d", &t->b.x, &t->b.y);
 }
 
-void print_line(Line rotated){
+void print_line(Line rotated) {
     printf("%d %d %d %d %.3lf", rotated.a.x, rotated.a.y, rotated.b.x, rotated.b.y, rotated.vector_len);
 }
 
-void rotate_line(Line * t){
+void rotate_line(Line *t) {
     double start_x = t->a.x;
 
     t->a.x = t->a.y;
@@ -49,6 +49,6 @@ void rotate_line(Line * t){
     t->b.y = -start_x;
 }
 
-void count_vector(Line * t){
+void count_vector(Line *t) {
     t->vector_len = sqrt(pow(t->b.x - t->a.x, 2) + pow(t->b.y - t->a.y, 2));
 }
